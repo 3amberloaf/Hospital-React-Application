704d4ae49b7777d924b71c2c2d7f83e2ee0bf015
Amber Sautner <ambersautner3@gmail.com>
2023-12-12 12:26:58 -0500

    made nurse unique

--- data/nurse.py
+++ data/nurse.py
@@ -2,6 +2,7 @@ import csv
 import random
 from faker import Faker
 from datetime import date, timedelta
+from uniqueID import generate_unique_id
 
 # Initialize Faker for generating fake data
 fake = Faker()
@@ -9,25 +10,31 @@ fake = Faker()
 # Define the column names
 columns = ['nurse_id', 'emp_id', 'shift', 'shiftDate', 'surgerySkill', 'name', 'years', 'surgeryType']
 
-
-# Generate random data for each column
-data = []
-for _ in range(100):  # You can adjust the number of rows as needed
-    nurse_id = random.randint(1, 100)
-    emp_id = random.randint(0, 100)
-    shift = random.choice(['Morning', 'Afternoon', 'Night'])
-    shiftDate = (date.today() + timedelta(days=random.randint(1, 30))).strftime('%Y-%m-%d')
-    surgerySkill = fake.random_element(elements=('Cardiology', 'Neurology', 'Orthopedics', 'General Surgery'))
-    name = fake.name()
-    years = random.randint(1, 30)
-    surgeryType = fake.random_element(elements=('Heart Surgery', 'Brain Surgery', 'Knee Replacement', 'Appendectomy'))
-
-    data.append([nurse_id, emp_id, shift, shiftDate, surgerySkill, name, years, surgeryType])
+nurse_id_set = set()
+emp_id_set = set()
 
 # Write the data to a CSV file
 with open('csv/nurse.csv', mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(columns)
-    writer.writerows(data)
+    writer = csv.DictWriter(file, fieldnames=columns)
+    writer.writeheader()
 
+    for _ in range(100):  # You can adjust the number of rows as needed
+        nurse_id = generate_unique_id(nurse_id_set, 100)
+        emp_id = generate_unique_id(emp_id_set, 100)
+        shift = random.choice(['Morning', 'Afternoon', 'Night'])
+        shiftDate = (date.today() + timedelta(days=random.randint(1, 30))).strftime('%Y-%m-%d')
+        surgerySkill = fake.random_element(elements=('Cardiology', 'Neurology', 'Orthopedics', 'General Surgery'))
+        name = fake.name()
+        years = random.randint(1, 30)
+        surgeryType = fake.random_element(elements=('Heart Surgery', 'Brain Surgery', 'Knee Replacement', 'Appendectomy'))
 
+        writer.writerow({
+            'nurse_id': nurse_id,
+            'emp_id': emp_id,
+            'shift': shift,
+            'shiftDate': shiftDate,
+            'surgerySkill': surgerySkill,
+            'name': name,
+            'years': years,
+            'surgeryType': surgeryType
+        })
