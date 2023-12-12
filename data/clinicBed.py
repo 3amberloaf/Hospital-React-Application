1e8ba707e38812ebe43ac8d9492f4c2f5572660a
Amber Sautner <ambersautner3@gmail.com>
2023-12-12 11:27:26 -0500

    updated clinicBed data

--- data/clinicBed.py
+++ data/clinicBed.py
@@ -3,23 +3,17 @@ import csv
 import string
 
 columns = ['bed_id', 'bed_letter', 'roomNumber', 'unit', 'wing']
-
-data = []
-
-# Define the range for bed_id and letters
-id_range = 100
 letters = string.ascii_uppercase
-
-for _ in range(100):
-    data.append([
-        random.randint(0, id_range),          # Random number for bed_id
-        random.choice(letters),                   # Random letter for bed_letter
-        random.randint(0, id_range),     # Random number for roomNumber (assuming room_number_range is defined)
-        random.choice(letters),                   # Random letter for unit
-        random.choice(letters)                    # Random letter for wing
-    ])
 
 with open('csv/clinicBed.csv', mode='w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(columns)
-    writer.writerows(data)
+    
+    for bed_id in range(1, 101):
+        writer.writerow([
+            bed_id,                                  # Sequential number for bed_id
+            random.choice(letters),                  # Random letter for bed_letter
+            bed_id,                 # Random number for roomNumber
+            random.choice(letters),                  # Random letter for unit
+            random.choice(letters)                   # Random letter for wing
+        ])
