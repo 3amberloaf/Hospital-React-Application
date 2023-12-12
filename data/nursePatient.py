f73fad53b3576456bacdd24326734fcee419c3de
Amber Sautner <ambersautner3@gmail.com>
2023-12-12 12:20:53 -0500

    made nursePatient unique

--- data/nursePatient.py
+++ data/nursePatient.py
@@ -1,23 +1,33 @@
 import csv
 import random
-from faker import Faker
 from datetime import date, timedelta
+from uniqueID import generate_unique_id
 
-fake = Faker()
 
 columns = ['nurse_id', 'patient_id', 'date', 'shift', 'idnursePatient']
 
-data = []
-for _ in range(100):
-    nurse_id = random.randint(1, 100)
-    patient_id = random.randint(0, 100)
-    shift = random.choice(['Morning', 'Afternoon', 'Night'])
-    admission_date = (date.today() - timedelta(days=random.randint(1, 30))).strftime('%Y-%m-%d')
-    idnursePatient = random.randint(1, 100)
-
-    data.append([nurse_id, patient_id, admission_date, shift, idnursePatient])
+nurse_id_set = set()
+patient_id_set = set()
+idnursePatient_set = set()
 
 with open('csv/Nurse_Patient.csv', mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(columns)
-    writer.writerows(data)
+    writer = csv.DictWriter(file, fieldnames=columns)
+    writer.writeheader()
+
+    for _ in range(100):
+        nurse_id = generate_unique_id(nurse_id_set, 100)
+        patient_id = generate_unique_id(patient_id_set, 100)
+        idnursePatient = generate_unique_id(idnursePatient_set, 100)
+        
+        nurse_id = random.randint(1, 100)
+        patient_id = random.randint(1, 100)
+        shift = random.choice(['Morning', 'Afternoon', 'Night'])
+        admission_date = (date.today() - timedelta(days=random.randint(1, 30))).strftime('%Y-%m-%d')
+
+        writer.writerow({
+            'nurse_id': nurse_id,
+            'patient_id': patient_id,
+            'date': admission_date,
+            'shift': shift,
+            'idnursePatient': idnursePatient
+        })
