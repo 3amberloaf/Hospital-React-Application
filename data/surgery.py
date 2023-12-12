43404267657046ce7d7b0eac940772d0d7f77346
Amber Sautner <ambersautner3@gmail.com>
2023-12-12 12:43:17 -0500

    surgery unqiue keys

--- data/surgery.py
+++ data/surgery.py
@@ -2,19 +2,25 @@ import csv
 import random
 from faker import Faker
 from datetime import date, timedelta
+from uniqueID import generate_unique_id  # You will need to define generate_unique_id
 
 fake = Faker()
 
 columns = ['surgery_id', 'surgeonID', 'typeID', 'date', 'theatre', 'patient_id']
 
+surgery_id_set = set()
+surgeon_id_set = set()
+typeID_set = set()
+patient_id_set = set()
+
 data = []
 for _ in range(100):
-    surgery_id = random.randint(0, 100)
-    surgeonID = random.randint(0, 100)
-    patient_id = random.randint(0, 100)
-    typeID = random.randint(7000, 7999)
+    surgery_id = generate_unique_id(surgery_id_set, 100)
+    surgeonID = generate_unique_id(surgeon_id_set, 100)
+    typeID = generate_unique_id(typeID_set, 999) + 7000  # Adjust the range and lower bound as needed
     surgery_date = (date.today() - timedelta(days=random.randint(1, 30))).strftime('%Y-%m-%d')
     theatre = fake.random_element(elements=('Operating Room 1', 'Operating Room 2', 'Operating Room 3'))
+    patient_id = generate_unique_id(patient_id_set, 100)
 
     data.append([surgery_id, surgeonID, typeID, surgery_date, theatre, patient_id])
 
