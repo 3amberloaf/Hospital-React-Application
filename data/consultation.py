e44f227a1dce16b04f4a3763b19f2a36bf7d8b31
Amber Sautner <ambersautner3@gmail.com>
2023-12-12 11:32:27 -0500

    added uniqueness to consultation

--- data/consultation.py
+++ data/consultation.py
@@ -2,30 +2,42 @@ import csv
 import random
 import pandas as pd
 
-
 columns = ['date', 'consultation_id', 'notes', 'patient_id', 'physician_id', 'type']
 
 # Placeholder data ranges
 date_range = pd.date_range(start='2021-01-01', end='2021-12-31').date
-id_range = 100
+id_range = 100  # Adjust this as needed to ensure enough unique IDs
 notes_samples = ['Follow-up in 3 months', 'Refer to specialist', 'Schedule surgery', 'Prescription refill', 'Physical therapy recommended']
 type_samples = ['Checkup', 'Emergency', 'Follow-up', 'Prescription', 'Surgery']
+
+# Sets to track unique IDs
+consultation_ids = set()
+patient_ids = set()
+physician_ids = set()
+
+# Function to generate a unique ID
+def generate_unique_id(existing_ids, id_range):
+    while True:
+        new_id = random.randint(1, id_range)
+        if new_id not in existing_ids:
+            existing_ids.add(new_id)
+            return new_id
 
 # Write to a new CSV file with the specified columns
 with open('csv/consultation.csv', mode='w', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=columns)
-    
-    # Write the header row
     writer.writeheader()
 
-    # Write 100 rows with random values for each column
     for _ in range(100):
-        writer.writerow({
-            'date': random.choice(date_range).strftime('%Y-%m-%d'),  # Random date formatted as string
-            'consultation_id': random.randint(1, id_range),      # Random id
-            'notes': random.choice(notes_samples),     # Random notes
-            'patient_id': random.randint(1, id_range),      # Random patient_id
-            'physician_id': random.randint(1, id_range),      # Random physician_id
-            'type': random.choice(type_samples)       # Random type
-        })
+        consultation_id = generate_unique_id(consultation_ids, id_range)
+        patient_id = generate_unique_id(patient_ids, id_range)
+        physician_id = generate_unique_id(physician_ids, id_range)
 
+        writer.writerow({
+            'date': random.choice(date_range).strftime('%Y-%m-%d'),
+            'consultation_id': consultation_id,
+            'notes': random.choice(notes_samples),
+            'patient_id': patient_id,
+            'physician_id': physician_id,
+            'type': random.choice(type_samples)
+        })
