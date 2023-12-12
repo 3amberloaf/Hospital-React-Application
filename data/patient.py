58241df7e75c10b0ceecf8d8c2f35f197f62557d
Amber Sautner <ambersautner3@gmail.com>
2023-12-12 12:32:32 -0500

    made patient unique

--- data/patient.py
+++ data/patient.py
@@ -2,24 +2,31 @@ import csv
 import random
 from faker import Faker
 from datetime import date, timedelta
-
+from uniqueID import generate_unique_id
 fake = Faker()
 
-columns = ['patient_id', 'name', 'address', 'addmittedDate', 'bedID', 'bloodType', 'dob', 'duration', 'gender', 'primaryPhysician', 'ssn']
+columns = ['patient_id', 'name', 'address', 'admittedDate', 'bedID', 'bloodType', 'dob', 'duration', 'gender', 'primaryPhysician', 'ssn']
+
+patient_id_set = set()
+bed_id_set = set()
+ssn_set = set()
+
+def generate_ssn(patient_id):
+    return f"{patient_id:03d}-00-{1000 + patient_id}"
 
 data = []
 for _ in range(100):
-    patient_id = random.randint(0, 100)
+    patient_id = generate_unique_id(patient_id_set, 100)  # You need to define generate_unique_id
     name = fake.name()
     address = fake.address()
     admission_date = (date.today() - timedelta(days=random.randint(1, 365))).strftime('%Y-%m-%d')
-    bedID = random.randint(1, 100)
+    bedID = generate_unique_id(bed_id_set, 100)  # You need to define generate_unique_id
     bloodType = random.choice(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])
     dob = fake.date_of_birth(minimum_age=18, maximum_age=90).strftime('%Y-%m-%d')
     duration = random.randint(1, 30)
     gender = random.choice(['Male', 'Female'])
     primaryPhysician = fake.name()
-    ssn = fake.ssn()
+    ssn = generate_ssn(patient_id)
 
     data.append([patient_id, name, address, admission_date, bedID, bloodType, dob, duration, gender, primaryPhysician, ssn])
 
